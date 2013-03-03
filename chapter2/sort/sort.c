8430da8db61401d1adb160cf5e19a176c6d35270
lyyyuna <lyyyuna@gmail.com>
2013-03-03 15:55:17 +0800

    add merge sort

--- chapter2/sort/sort.c
+++ chapter2/sort/sort.c
@@ -17,3 +17,38 @@ void insertion_sort(int *array, int size)
         *(array+i+1) = key;
     }
 }
+
+void merge(int *array, int p, int q, int r)
+{
+    int arr_l[q-p+1], arr_r[r-q+1];
+    int i, j, n1, n2, k;
+
+    n1 = q-p+1;
+    n2 = r-q;
+    for (i = 0; i < n1; i++)
+        arr_l[i] = array[p+i];
+    for (j = 0; j < n2; j++)
+        arr_r[j] = array[q+j+1];
+
+    i = j = 0;
+    for (k = p; k < r+1; k++)
+    {
+        if ((arr_l[i] <= arr_r[j] || j>=n2) && i<n1)
+            array[k] = arr_l[i++];
+        else
+            array[k] = arr_r[j++];
+    }
+}
+
+void merge_sort(int *array, int p, int r)
+{
+    int m = 0;
+
+    if (p < r)
+    {
+        m = (p+r)/2;
+        merge_sort(array, p, m);
+        merge_sort(array, m+1, r);
+        merge(array, p, m, r);
+    }
+}
