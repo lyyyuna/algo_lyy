e24348c64e0b3f63634b25105e0c50347dca3ad8
lyyyuna <lyyyuna@gmail.com>
2013-03-03 15:33:03 +0800

    delete some declaration

--- chapter2/sort/sort.h
+++ chapter2/sort/sort.h
@@ -2,8 +2,7 @@
 #define SORT_INCLUDED
 
 extern void insertion_sort(int *array, int size);
-extern void reverse_insertion_sort(int *array, int size);
-extern void selection_sort(int *array, int size);
+
 extern void merge(int *array, int a, int b, int c);
 extern void merge_sort(int *array, int a, int b);
 
